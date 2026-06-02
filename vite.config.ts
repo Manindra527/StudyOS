261b53682f86c218b1893c2827674900951ecd64
Manindra527 <manindra856@gmail.com>
2026-06-02 14:32:10 +0530

    Initial commit3

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
   },
 });*/
 export default defineConfig({
-  nitro: true,
+  nitro: {
+    preset: "node-server",
+  },
   tanstackStart: {
     server: {
       entry: "server",
